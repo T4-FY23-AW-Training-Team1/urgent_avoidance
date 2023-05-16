6b933d79c9cab4deca9637207bb8c1fd9fcc4701
TaikiYamada4 <taiki.yamada@tier4.jp>
2023-05-16 14:20:57 +0900

    Added a feature to find the nearest road shoulder lanelet, and set the goal to the center of it.

--- urgent_avoidance/src/urgent_avoidance.cpp
+++ urgent_avoidance/src/urgent_avoidance.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <memory>
+#include <lanelet2_extension/utility/utilities.hpp>
+#include <lanelet2_extension/utility/query.hpp>
+#include <lanelet2_extension/utility/message_conversion.hpp>
+#include <autoware_auto_mapping_msgs/msg/had_map_bin.hpp>
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
@@ -14,8 +18,12 @@ class UrgentAvoidancePlanner : public rclcpp::Node
     public:
         UrgentAvoidancePlanner() : Node("urgent_avoidance_planner")
         {
-            subscription_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+            pose_subscription_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
             "~/input/pose", 10, std::bind(&UrgentAvoidancePlanner::messageCallback, this, std::placeholders::_1));
+
+            map_bin_subscription_ = this->create_subscription<autoware_auto_mapping_msgs::msg::HADMapBin>(
+            "~/input/lanelet2_map", rclcpp::QoS{1}.transient_local(),
+            std::bind(&UrgentAvoidancePlanner::onMapBin, this, std::placeholders::_1));
 
             service_ = this->create_service<FindRoadShoulderGoal>(
                 "/provoke_avoidance",
@@ -31,6 +39,16 @@ class UrgentAvoidancePlanner : public rclcpp::Node
             last_received_msg_ = msg;
         }
 
+        void onMapBin(const autoware_auto_mapping_msgs::msg::HADMapBin::ConstSharedPtr msg){
+            lanelet::LaneletMapPtr ll_map(new lanelet::LaneletMap);
+            lanelet::utils::conversion::fromBinMsg(*msg, ll_map);
+            RCLCPP_INFO(this->get_logger(), "Map is loaded\n");
+
+            // get Lanelets
+            all_lanelets = lanelet::utils::query::laneletLayer(ll_map);
+            shoulder_lanelets = lanelet::utils::query::shoulderLanelets(all_lanelets);
+        }
+
         void calc_goal_and_publish(
             const std::shared_ptr<FindRoadShoulderGoal::Request> request,
             std::shared_ptr<FindRoadShoulderGoal::Response> response)
@@ -38,7 +56,8 @@ class UrgentAvoidancePlanner : public rclcpp::Node
             if(last_received_msg_)
             {
                 request->current_pose = *last_received_msg_;
-                response->goal_pose = advance_one_meter(request->current_pose);
+                //response->goal_pose = advance_one_meter(request->current_pose);
+                response->goal_pose = set_goal_to_closest_road_shoulder(request->current_pose);
                 RCLCPP_INFO(this->get_logger(), "Received pose, calculating next goal");
                 publisher_->publish(response->goal_pose);
                 RCLCPP_INFO(this->get_logger(), "Objective goal calculated and sent");
@@ -76,10 +95,31 @@ class UrgentAvoidancePlanner : public rclcpp::Node
             return new_pose_stamped;
         }
 
-        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr subscription_;
+        geometry_msgs::msg::PoseStamped set_goal_to_closest_road_shoulder(const geometry_msgs::msg::PoseStamped &current_pose_stamped)
+        {
+            // remove stamp
+            geometry_msgs::msg::Pose current_pose;
+            geometry_msgs::msg::PoseStamped shoulder_pose_stamped;
+            current_pose.position = current_pose_stamped.pose.position;
+            current_pose.orientation = current_pose_stamped.pose.orientation;
+
+            // get closest shoulder lanelet
+            lanelet::ConstLanelet closest_shoulder_lanelet;
+            if(lanelet::utils::query::getClosestLanelet(shoulder_lanelets, current_pose, &closest_shoulder_lanelet)){
+                shoulder_pose_stamped.pose = lanelet::utils::getClosestCenterPose(closest_shoulder_lanelet, current_pose.position);
+                shoulder_pose_stamped.header = current_pose_stamped.header;
+            }
+
+            return shoulder_pose_stamped;
+        }
+
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_subscription_;
+        rclcpp::Subscription<autoware_auto_mapping_msgs::msg::HADMapBin>::SharedPtr map_bin_subscription_;
         rclcpp::Service<FindRoadShoulderGoal>::SharedPtr service_;
         rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher_;
         geometry_msgs::msg::PoseStamped::SharedPtr last_received_msg_;
+        lanelet::ConstLanelets all_lanelets;
+        lanelet::ConstLanelets shoulder_lanelets;
 };
 
 int main(int argc, char *argv[])
